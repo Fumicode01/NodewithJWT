bdcbca840fbbee60939f2406f9b662b758f5736e
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2021-10-20 21:54:21 +1100

    Connected to DB
    install mongoose
    connected to DB with mongoose

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+// conect DB
+mongoose.connect("mongodb+srv://fumi:Fumi1102@cluster0.48qtj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
+()=> console.log("connected to DB!"));
 
 // Import Routes
 const authRoute = require("./routes/auth");

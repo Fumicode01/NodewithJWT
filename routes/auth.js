b41a3e507cac3bd6584611d4fe1d8191a03ae382
Fumicode01 <65881354+Fumicode01@users.noreply.github.com>
2021-10-21 23:23:43 +1100

    add validation for existin email address

--- routes/auth.js
+++ routes/auth.js
@@ -9,6 +9,12 @@ const {loginValidation} = require('./validation');
 router.post("/register",  async (req, res) => {
   const {error} = registerValidation(req.body);
   if(error) return res.status(400).send(error.details[0].message);
+
+
+  // Check if the user is already in the database
+  const emailexist = await User.findOne({email:req.body.email});
+  if(emailexist) return res.status(400).send("Email already Exist");
+
   const user = new User({
     name:req.body.name,
     email:req.body.email,
